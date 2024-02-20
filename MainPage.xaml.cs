b455b3afbc5dd53d1e79abe408689324fb0d9db5
Cesar Vicente <cesar_vicente@outlook.com.br>
2024-02-20 02:00:21 -0300

    Finally

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,4 +6,48 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private async void OnGenerateLuckNumbers()
+	{
+		btSort.IsVisible = false;
+        NameApp.IsVisible = false;
+		ContainerLuckNumbers.IsVisible = true;
+		var listNumbers = getRandomListNumbers();
+
+		LuckNumber01.Text = $"{listNumbers.ElementAt(0):D2}";
+        await LuckNumber01.RotateTo(LuckNumber01.Rotation + 360, 500, Easing.BounceOut);
+
+		LuckNumber02.Text = $"{listNumbers.ElementAt(1):D2}";
+        await LuckNumber02.RotateTo(LuckNumber02.Rotation + 360, 500, Easing.BounceOut);
+
+		LuckNumber03.Text = $"{listNumbers.ElementAt(2):D2}";
+        await LuckNumber03.RotateTo(LuckNumber03.Rotation + 360, 500, Easing.BounceOut);
+
+        LuckNumber04.Text = $"{listNumbers.ElementAt(3):D2}";
+        await LuckNumber04.RotateTo(LuckNumber04.Rotation + 360, 500, Easing.BounceOut);
+
+		LuckNumber05.Text = $"{listNumbers.ElementAt(4):D2}";
+        await LuckNumber05.RotateTo(LuckNumber05.Rotation + 360, 500, Easing.BounceOut);
+
+		LuckNumber06.Text = $"{listNumbers.ElementAt(5):D2}";
+        await LuckNumber06.RotateTo(LuckNumber06.Rotation + 360, 500, Easing.BounceOut);
+        btSort.IsVisible = true;
+    }
+
+	private SortedSet<int> getRandomListNumbers()
+	{
+		var listNumbers = new SortedSet<int>();
+
+		while(listNumbers.Count < 6)
+		{
+            listNumbers.Add(new Random().Next(1, 60));
+		}
+
+		return listNumbers;
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		OnGenerateLuckNumbers();
+    }
 }
